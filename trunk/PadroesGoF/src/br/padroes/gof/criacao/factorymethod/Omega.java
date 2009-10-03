9b15c856cc48e1f35b164249da1fbe26504118d4
danielmenezes <danielmenezes@ebdf812c-b01e-11de-a2ab-0da4ed5138bb>
2009-10-03 14:34:27 +0000

    Exemplo do factory method.

---
+++ trunk/PadroesGoF/src/br/padroes/gof/criacao/factorymethod/Omega.java
@@ -0,0 +1,10 @@
+package br.padroes.gof.criacao.factorymethod;
+
+public class Omega extends Carro {
+
+	@Override
+	public double getPreco() {
+		return 35000;
+	}
+
+}
---
+++ trunk/PadroesGoF/src/br/padroes/gof/criacao/factorymethod/Vectra.java
@@ -0,0 +1,10 @@
+package br.padroes.gof.criacao.factorymethod;
+
+public class Vectra extends Carro {
+
+	@Override
+	public double getPreco() {
+		return 40000;
+	}
+
+}
