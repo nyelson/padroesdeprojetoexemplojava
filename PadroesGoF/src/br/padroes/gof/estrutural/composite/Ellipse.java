f8cafeafabc338336d150c0c78e9a9d659b0a245
danielmenezes <danielmenezes@ebdf812c-b01e-11de-a2ab-0da4ed5138bb>
2009-10-14 23:58:13 +0000

    Upload Composite

---
+++ PadroesGoF/src/br/padroes/gof/estrutural/composite/Ellipse.java
@@ -0,0 +1,11 @@
+package br.padroes.gof.estrutural.composite;
+
+/** "Leaf" */
+class Ellipse implements Graphic {
+ 
+    //Prints the graphic.
+    public void print() {
+        System.out.println("Ellipse");
+    }
+ 
+}
