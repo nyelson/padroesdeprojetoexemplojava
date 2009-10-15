02e9729584cf7f2fd934221003006afa5abe0f0e
danielmenezes <danielmenezes@ebdf812c-b01e-11de-a2ab-0da4ed5138bb>
2009-10-15 00:31:48 +0000

    update

--- trunk/PadroesGoF/src/br/padroes/gof/criacao/sigleton/SingletonLog.java
+++ trunk/PadroesGoF/src/br/padroes/gof/criacao/sigleton/SingletonLog.java
@@ -18,6 +18,23 @@ public class SingletonLog {
         return SingletonLogHolder.instance;
     }
 
+    /**
+     * University of Maryland Computer Science researcher Bill Pugh has written about the code issues 
+     * underlying the Singleton pattern when implemented in Java.[8] Pugh's efforts on the 
+     * "Double-checked locking" idiom led to changes in the Java memory model in Java 5 
+     * and to what is generally regarded as the standard method to implement Singletons in Java. 
+     * The technique is known as the initialization on demand holder idiom, is as lazy as possible, 
+     * and works in all known versions of Java. It takes advantage of language guarantees about 
+     * class initialization, and will therefore work correctly in all Java-compliant compilers and 
+     * virtual machines.
+     * 
+     * The inner class is referenced no earlier (and therefore loaded no earlier by the class loader) 
+     * than the moment that getInstance() is called. Thus, this solution is thread-safe without 
+     * requiring special language constructs (i.e. volatile or synchronized).
+     *    
+     * SingletonHolder is loaded on the first execution of Singleton.getInstance() 
+     * or the first access to SingletonHolder.INSTANCE, not before.
+     */
     //Classe auxiliar para criacao da instancia. Evita problemas de sincronizacao de threads.
     private static class SingletonLogHolder {
            private static SingletonLog instance = new SingletonLog();
