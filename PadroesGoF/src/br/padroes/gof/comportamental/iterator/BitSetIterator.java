1a81efef5a48022114737103d10f6ea1f6799333
danielmenezes <danielmenezes@ebdf812c-b01e-11de-a2ab-0da4ed5138bb>
2009-11-26 12:40:41 +0000

    iterator

---
+++ PadroesGoF/src/br/padroes/gof/comportamental/iterator/BitSetIterator.java
@@ -0,0 +1,30 @@
+package br.padroes.gof.comportamental.iterator;
+
+import java.util.BitSet;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BitSetIterator implements Iterator<Boolean> { 
+	private final BitSet bitset; 
+	private int index;
+ 
+	public BitSetIterator(BitSet bitset) { 
+		this.bitset = bitset; 
+	}
+ 
+	public boolean hasNext() {   
+		return index < bitset.length(); 
+	} 
+ 
+	public Boolean next() { 
+		if (index >= bitset.length()) { 
+			throw new NoSuchElementException(); 
+		} 
+		boolean b = bitset.get(index++); 
+		return new Boolean(b); 
+	} 
+ 
+	public void remove() { 
+		throw new UnsupportedOperationException(); 
+	}
+}
